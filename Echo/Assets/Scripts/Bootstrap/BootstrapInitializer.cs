c4c22152c1f411daee001d608c0ba74f5a34aaa7
Huub van de Hoef <Huub_thomas@hotmail.com>
2021-09-14 20:44:17 +0200

    Added base project

---
+++ Echo/Assets/Scripts/Bootstrap/BootstrapInitializer.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class BootstrapInitializer : MonoBehaviour
+{
+    void Awake()
+    {
+        SceneManager.sceneLoaded += FirstSceneLoaded;
+        SceneManager.LoadSceneAsync(1, LoadSceneMode.Single);//Load menu scene
+
+        //Load loading screen scene
+        SceneManager.LoadSceneAsync(2, LoadSceneMode.Additive);
+    }
+
+	private void FirstSceneLoaded(Scene arg0, LoadSceneMode arg1)
+	{
+        SceneManager.sceneLoaded -= FirstSceneLoaded;
+        Debug.Log($"Scene Loaded");
+    }
+}
---
+++ Echo/Assets/Scripts/Loading/LoadingController.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LoadingController : MonoBehaviour
+{
+    public static LoadingController Instance;
+
+	[SerializeField]private GameObject _loadingScreen;
+
+    void Awake()
+    {
+		if(Instance!= null)
+		{
+			Destroy(this);
+		}
+		else
+		{
+			Instance = this;
+		}
+    }
+
+   public void ToggleLoadingScreen(bool toggle)
+	{
+		_loadingScreen.SetActive(toggle);
+	}
+
+}
