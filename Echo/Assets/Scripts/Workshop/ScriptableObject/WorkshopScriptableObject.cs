3cb45a11254c2ee3633d950d42ab12687467801c
Huub van de Hoef <Huub_thomas@hotmail.com>
2021-09-14 23:15:41 +0200

    Added scenes and basic ui

---
+++ Echo/Assets/Scripts/Workshop/ScriptableObject/WorkshopScriptableObject.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu(fileName = "WorkshopData", menuName = "ScriptableObjects/SpawnManagerScriptableObject", order = 1)]
+public class WorkshopScriptableObject : ScriptableObject
+{
+	public List<WorkshopItem> WorkshopItems;
+}
+
+[System.Serializable]
+public class WorkshopItem
+{
+	public GameObject WorkshopPrefab;
+	public Sprite WorkshopImage;
+	public string ItemName;
+	public string ItemDescription;
+	public int Price;
+}
---
+++ Echo/Assets/Scripts/Workshop/UI/WorkshopTypeSelectButton.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using UnityEngine;
+using UnityEngine.UI;
+using TMPro;
+
+[RequireComponent(typeof(Button))]
+public class WorkshopTypeSelectButton : MonoBehaviour
+{
+	[SerializeField]private string _sectionName;
+	[SerializeField] private string _scriptableLoadName;
+
+	private WorkshopScriptableObject _loadedObjects;
+
+	private void Awake()
+	{
+		GetComponent<Button>().onClick.AddListener(OnClick);
+		_loadedObjects = Resources.Load<WorkshopScriptableObject>($"Workshop/{_scriptableLoadName}");
+
+		//Create a list of items and dissable it
+	}
+
+	private void OnClick()
+	{
+		//Set this as active
+	}
+}
